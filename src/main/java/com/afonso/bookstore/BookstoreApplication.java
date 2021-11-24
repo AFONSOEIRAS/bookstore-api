a869305426e9b20860e86869433580037f342447
AFONSOEIRAS <afonsoeiras26@gmail.com>
2021-11-24 08:28:27 -0300

    criacao do perfil de DEV

--- src/main/java/com/afonso/bookstore/BookstoreApplication.java
+++ src/main/java/com/afonso/bookstore/BookstoreApplication.java
@@ -12,26 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
+public class BookstoreApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
 
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "informática", "Livros de TI");
 
-		Livro l1 = new Livro(null,"Clean code","Robert Martin", "Lorem impsum", cat1);
-
-		cat1.getLivros().addAll(Arrays.asList(l1));
-
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-	}
 }
